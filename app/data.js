a68e8d4816ebb735998d24ea540183b6357a65c2
rithi <krishnamchan@gmail.com>
2023-06-18 19:06:39 +0530

    first commit

---
+++ app/data.js
@@ -0,0 +1,45 @@
+export const quiz = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: "What is the most useless thing?",
+      answers: [
+        "krish's life",
+        "Nayanthara in Bigil",
+        "Engineering Degree",
+        "Sricant",
+      ],
+      correctAnswer: "Sricant",
+    },
+    {
+      id: 2,
+      question: "What is the biggest lie in our solar system?",
+      answers: [
+        "unkooda nooru varusham vaazhanum",
+        "machan dho vanten da",
+        'Sricant\'s "pannirlaam sir" ',
+        "ipo mattu kasta padu, aprm jolly dhan",
+      ],
+      correctAnswer: 'Sricant\'s "pannirlaam sir" ',
+    },
+    {
+      id: 3,
+      question: "What is the smallest thing in the world?",
+      answers: ["Ant", "Grain of sand", "Atom", "S......"],
+      correctAnswer: "S......",
+    },
+    {
+      id: 4,
+      question: "What is the most widely spoken language in the world?",
+      answers: ["English", "Mandarin", "Spanish", "chowk chowk"],
+      correctAnswer: "chowk chowk",
+    },
+    {
+      id: 5,
+      question: "Who is the founder of Quantum Mechanics Field?",
+      answers: ["Steve Jobs", "Bill Gates", "Elon Musk", "Sricant"],
+      correctAnswer: "Sricant",
+    },
+  ],
+};
